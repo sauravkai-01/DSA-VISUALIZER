31db75d2eceabc9a4f62e299c21658cb391072d5
Saurav Kumar <sauravkai2023@gmail.com>
2026-05-20 14:56:17 +0530

    Remove Next.js configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
 
-export default nextConfig
